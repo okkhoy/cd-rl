2eaa1102825f5b96e026ce2208260c6251aa5796
Akshay Narayan <okkhoy@gmail.com>
2018-06-15 18:07:32 +0800

    Add log messages to fourier.py

--- representation/fourier.py
+++ representation/fourier.py
@@ -1,5 +1,6 @@
 import numpy
 import trivial
+import logging
 import itertools
 
 
@@ -15,15 +16,21 @@ class FourierBasis(trivial.TrivialBasis):
     """
 
     def __init__(self, nvars, ranges, order=3):
+        log = logging.getLogger('pyrl.representation.fourier')
         nterms = pow(order + 1, nvars)
         self.numTerms = nterms
         self.order = order
         self.ranges = numpy.array(ranges)
         iter = itertools.product(range(order + 1), repeat=nvars)
         self.multipliers = numpy.array([list(map(int, x)) for x in iter])
+        log.debug("Numterms: %d   Order: %d \n Ranges: %s", self.numTerms, self.order, self.ranges)
 
     def computeFeatures(self, features):
+        log = logging.getLogger('pyrl.representation.fourier.computeFeatures')
         if len(features) == 0:
             return numpy.ones((1,))
         basisFeatures = numpy.array([self.scale(features[i], i) for i in range(len(features))])
-        return numpy.cos(numpy.pi * numpy.dot(self.multipliers, basisFeatures))
+        log.info("Features: %s", features)
+        log.info("Basis Features: %s", basisFeatures)
+        # return_val = numpy.cos(numpy.pi * numpy.dot(self.multipliers, basisFeatures))
+        return numpy.cos(numpy.pi * numpy.dot(self.multipliers, basisFeatures))  # return_val
