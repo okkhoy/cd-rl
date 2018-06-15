7e77a39ef7cf011cc71b5469d7e88ef441f960e7
Akshay Narayan <okkhoy@gmail.com>
2018-06-15 10:51:25 +0800

    Code formatting for PEP8: environments.BernoulliEnvironment.py

--- environments/BernoulliEnvironment.py
+++ environments/BernoulliEnvironment.py
@@ -1,33 +1,32 @@
-"""
-Created on May 7, 2017
+# Created on May 7, 2017
+#
+# @author: Akshay Narayan
+#
+# This code is shared under The MIT License
+# -----------------------------------------
+#
+# The MIT License (MIT)
+#
+# Copyright (c) <year> <copyright holders>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
-@author: Akshay Narayan
-
-This code is shared under The MIT License
------------------------------------------
-
-The MIT License (MIT)
-
-Copyright (c) <year> <copyright holders>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
 
 import random
 
@@ -36,8 +35,8 @@ from rlglue.environment import EnvironmentLoader as EnvironmentLoader
 from rlglue.types import Observation
 from rlglue.types import Action
 from rlglue.types import Reward_observation_terminal
-from rlglue import TaskSpecRLGlue
-from rlglue.registry import register_environment
+from local_rlglue import TaskSpecRLGlue
+from local_rlglue.registry import register_environment
 
 
 class BernoulliArm:
@@ -71,7 +70,7 @@ class BernoulliEnv(Environment):
 
     def makeTaskSpec(self):
         ts = TaskSpecRLGlue.TaskSpec(discount_factor=0.9, reward_range=(0.0, 1.0))
-        ts.addDiscreteAction((0, len(self.means)-1))
+        ts.addDiscreteAction((0, len(self.means) - 1))
         ts.addDiscreteObservation((0, 1))
         ts.setEpisodic()
         ts.setExtra(self.name)
